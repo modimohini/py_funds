1f848238b49dd6a7eefee20383f92f25eb2b60f9
mohini modi <mohini.modi@resmed.com>
2021-03-20 21:22:33 -0700

    py fun

--- divisible.py
+++ divisible.py
@@ -1,3 +1,9 @@
 """Given a list of numbers, Iterate it and print only those numbers which are divisible of 5"""
 given_list = [10, 20, 33, 46, 55]
 
+for x in given_list:
+   
+    if ( x % 5  == 0):
+        print("devisible num " , x)
+    else:
+        print("not divisible by 5 ", "given number ", x )
