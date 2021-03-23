517e3f790d818bb46b669a66659a387052ad78fd
mohini modi <mohini.modi@resmed.com>
2021-03-23 11:27:15 -0700

    while

---
+++ baseExponent.py
@@ -0,0 +1,11 @@
+"""Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp."""
+base = 5
+exponent = 4
+result = 1
+num = exponent
+
+while num > 0:
+    result = result * base 
+    num = num - 1
+    print(result)
+
